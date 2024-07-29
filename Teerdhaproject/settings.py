a40f28773adec416bc5243e98e47b53bad3e4fb9
Praveenchinna14 <162446658+Praveenchinna14@users.noreply.github.com>
2024-07-29 17:29:10 +0530

    Update settings.py

--- Teerdhaproject/settings.py
+++ Teerdhaproject/settings.py
@@ -84,9 +84,9 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME':'teerdha20',
         'PORT':'3306',
-        'PASSWORD':'praveen',
+        'PASSWORD':'admin12345',
         'HOST':'my-rds-instance.ctoc6i4sess4.us-east-2.rds.amazonaws.com',
-        'USER':'admin12345'
+        'USER':'praveen'
     }
 }
 
